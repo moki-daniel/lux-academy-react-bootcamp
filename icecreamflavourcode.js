5e1c2af56f8f9191f8d8fd90251fae7c3509eeda
Moki Daniel <mokidnl@gmail.com>
2020-12-18 15:21:59 +0300

    ice cream flavor code

---
+++ icecreamflavourcode.js
@@ -0,0 +1,11 @@
+
+// change the values of `flavor`, `vessel`, and `toppings` to test your code
+var flavor = "vanilla";
+var vessel = "cone";
+var toppings = "cookies";
+
+// Add your code here
+if ((flavor === "vanilla" || flavor === "chocolate") && 
+(vessel === "cone" || vessel === "bowl") && (toppings === "sprinkles" || toppings === "peanuts")) {
+        console.log ("I'd like two scoops of " + flavor + "ice cream in a " + vessel + "with " + toppings + ".")
+}
