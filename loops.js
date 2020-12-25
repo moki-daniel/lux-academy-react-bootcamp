eee1ce28df7fb077dbf90257f0bcf1fcfa8d91bd
Moki Daniel <mokidnl@gmail.com>
2020-12-25 16:24:48 +0300

    a sample of a loop function

---
+++ loops.js
@@ -0,0 +1,43 @@
+/* 
+Directions:
+Write a loop that prints out the following song. Starting at 99, and ending at 1 bottle.
+
+99 bottles of juice on the wall! 99 bottles of juice! Take one down, pass it around... 98 
+bottles of juice on the wall!
+98 bottles of juice on the wall! 98 bottles of juice! Take one down, pass it around... 97
+ bottles of juice on the wall!
+...
+2 bottles of juice on the wall! 2 bottles of juice! Take one down, pass it around... 1 bottle 
+of juice on the wall!
+1 bottle of juice on the wall! 1 bottle of juice! Take one down, pass it around... 0 bottles 
+of juice on the wall!
+Some Notes:
+
+Note the pluralization of the word "bottle" when you go from 2 bottles to 1 bottle.
+Your text editor may try to autocorrect your ellipses (...) to the ellipses character (…). 
+Do not use the ellipses character for this quiz; use three consecutive periods instead.
+*/
+
+var num = 99;
+
+while (num >=1) {
+    // Last iteration. Note occurances of bottle, bottle, bottleS
+    if (num === 1) {
+        console.log(num + " bottle of juice on the wall! "
+                    + num + " bottle of juice! Take one down, pass it around... "
+                    + (num-1) + " bottles of juice on the wall!");
+    }
+    // Second-last iteration. Note occurances of bottleS, bottleS, bottle
+    else if (num === 2){
+        console.log(num + " bottles of juice on the wall! "
+                    + num + " bottles of juice! Take one down, pass it around... "
+                    + (num-1) + " bottle of juice on the wall!");
+    }
+    // All other iterations. Note occurances of bottleS, bottleS, bottleS
+    else {
+        console.log(num + " bottles of juice on the wall! "
+                    + num + " bottles of juice! Take one down, pass it around... "
+                    + (num-1) + " bottles of juice on the wall!");
+    }
+    num = num - 1;
+}
